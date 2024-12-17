34ef01e4243acf3079fc86f0fcd08238a81f7c60
Loay Halawani <loayhalawani0@gmail.com>
2024-12-17 18:52:08 +0200

    fixes

--- Graph.java
+++ Graph.java
@@ -4,13 +4,10 @@ public class Graph {
 
 	private Map<Integer, List<Integer>> adj;
 	List<Integer> visited;
-	private int V;
 
 	// Undirected Graph
-	public Graph(int V) {
-		this.V = V;
-		adj = new HashMap<>(V);
-		visited = new ArrayList<>();
+	public Graph() {
+		adj = new HashMap<>();
 	}
 
 	private void addEdge(int v, int u) {
@@ -23,6 +20,7 @@ public class Graph {
 	// BFS
 	private void BFS() {
 		Queue<Integer> queue = new LinkedList<>();
+		visited = new ArrayList<>();
 		
 		int firstVertex = adj.keySet().iterator().next();
 		visited.add(firstVertex);
@@ -61,15 +59,22 @@ public class Graph {
 
 	// Driver
 	public static void main(String[] args) {
-		Graph graph = new Graph(7);
+		Graph graph = new Graph();
 
-		graph.addEdge(1, 5);
-		graph.addEdge(1, 4);
 		graph.addEdge(1, 2);
+		graph.addEdge(1, 4);
+		graph.addEdge(2, 5);
 		graph.addEdge(2, 7);
-		graph.addEdge(2, 6);
+		graph.addEdge(2, 8);
 		graph.addEdge(2, 3);
-		graph.addEdge(5, 13);
+		graph.addEdge(3, 4);
+		graph.addEdge(3, 10);
+		graph.addEdge(3, 9);
+		graph.addEdge(5, 6);
+		graph.addEdge(5, 7);
+		graph.addEdge(5, 8);
+		graph.addEdge(7, 8);
+
 
 		System.out.print("BFS: ");
 
